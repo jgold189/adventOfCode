4f99b8d04b171649843bd99ec41f794c7973e6c3
jordan <climbjn189@gmail.com>
2021-11-09 23:08:31 -0500

    Finished part 2, day 1

--- 2018/go/01/main.go
+++ 2018/go/01/main.go
@@ -18,15 +18,24 @@ func getStringInput() string {
 func solve(input string) int {
 	splitInput := strings.Split(input, "\n")
 
-	result := 0
-	for i := range splitInput {
-		num, err := strconv.Atoi(splitInput[i])
-		if err != nil {
-			panic(err)
+	resultsMap := make(map[int]int)
+	runningCount := 0
+	for {
+		for i := range splitInput {
+			num, err := strconv.Atoi(splitInput[i])
+			if err != nil {
+				panic(err)
+			}
+			runningCount += int(num)
+
+			num, ok := resultsMap[runningCount]
+			if ok {
+				return runningCount
+			} else {
+				resultsMap[runningCount] = 1
+			}
 		}
-		result += int(num)
 	}
-	return result
 }
 
 func main() {
--- 2018/go/01/main_test.go
+++ 2018/go/01/main_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func TestSolveInput1(t *testing.T) {
-	input := "+1\n+1\n+1"
-	answer := 3
+	input := "+1\n-1"
+	answer := 1
 	result := solve(input)
 	if result != answer {
 		t.Fatalf(`solve is wrong. Wanted %d, got %d`, answer, result)
@@ -14,8 +14,8 @@ func TestSolveInput1(t *testing.T) {
 }
 
 func TestSolveInput2(t *testing.T) {
-	input := "+1\n+1\n-2"
-	answer := 0
+	input := "+3\n+3\n+4\n-2\n-4"
+	answer := 10
 	result := solve(input)
 	if result != answer {
 		t.Fatalf(`solve is wrong. Wanted %d, got %d`, answer, result)
@@ -23,8 +23,17 @@ func TestSolveInput2(t *testing.T) {
 }
 
 func TestSolveInput3(t *testing.T) {
-	input := "-1\n-2\n-3"
-	answer := -6
+	input := "-6\n+3\n+8\n+5\n-6"
+	answer := 5
+	result := solve(input)
+	if result != answer {
+		t.Fatalf(`solve is wrong. Wanted %d, got %d`, answer, result)
+	}
+}
+
+func TestSolveInput4(t *testing.T) {
+	input := "+7\n+7\n-2\n-7\n-4"
+	answer := 14
 	result := solve(input)
 	if result != answer {
 		t.Fatalf(`solve is wrong. Wanted %d, got %d`, answer, result)
