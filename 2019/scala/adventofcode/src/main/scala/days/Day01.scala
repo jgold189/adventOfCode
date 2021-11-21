05541173ab53d9699c667e2b936d739b92a0f200
jordan <climbjn189@gmail.com>
2021-11-21 18:03:48 -0500

    Scala scaffold

---
+++ 2019/scala/adventofcode/src/main/scala/days/Day01.scala
@@ -0,0 +1,12 @@
+object Day01 {
+    
+    def getSquareString(input: Double): String = {
+        val square = input * input
+        square.toString
+    }
+
+    def solveDay() = {
+        println("1")
+    }
+
+}
