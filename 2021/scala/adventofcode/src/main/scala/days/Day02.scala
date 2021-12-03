721f116e20603013f140c57e7020e63165c8df5e
jordan <climbjn189@gmail.com>
2021-12-02 23:47:01 -0500

    Day 2 Part 2

--- 2021/scala/adventofcode/src/main/scala/days/Day02.scala
+++ 2021/scala/adventofcode/src/main/scala/days/Day02.scala
@@ -3,18 +3,18 @@ import scala.annotation.tailrec
 object Day02 {
 
     // forward 1
-    def takeAction(currVal: (Int, Int), action: String): (Int, Int) = {
-        val (x, y) = currVal
+    def takeAction(currVal: (Int, Int, Int), action: String): (Int, Int, Int) = {
+        val (x, y, aim) = currVal
         val actionSplit = action.split(" ")
         actionSplit match {
-            case Array("forward", _) =>(x + actionSplit(1).toInt, y)
-            case Array("down", _) => (x, y + actionSplit(1).toInt)
-            case Array("up", _) => (x, y - actionSplit(1).toInt)
+            case Array("forward", _) =>(x + actionSplit(1).toInt, y + (aim * actionSplit(1).toInt), aim)
+            case Array("down", _) => (x, y, aim + actionSplit(1).toInt)
+            case Array("up", _) => (x, y, aim - actionSplit(1).toInt)
         }
     }
 
     def calcSubPos(input: Vector[String]): Int = {
-        val (x, y) = input.foldLeft((0, 0))(takeAction)
+        val (x, y, _) = input.foldLeft((0, 0, 0))(takeAction)
         x * y
     }
 
--- 2021/scala/adventofcode/src/test/scala/days/Day02Test.scala
+++ 2021/scala/adventofcode/src/test/scala/days/Day02Test.scala
@@ -3,6 +3,6 @@ import org.scalatest.funsuite.AnyFunSuite
 class Day02Test extends AnyFunSuite {
       test("Day02.calcSubPos") {
           val data = Vector("forward 5","down 5","forward 8","up 3","down 8","forward 2")
-          assert(Day02.calcSubPos(data) === 150)
+          assert(Day02.calcSubPos(data) === 900)
       }
 }
