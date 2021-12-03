08112383e4e83e64b1fc73b652e8e018d46fd2d5
jordan <climbjn189@gmail.com>
2021-12-03 00:37:39 -0500

    Day 3 Part 2

--- 2021/python/day03.py
+++ 2021/python/day03.py
@@ -1,15 +1,46 @@
 from aocUtil import *
 from collections import Counter
+from copy import deepcopy
 
+# This is some really bad code because I was trying to do this as fast as possible
 def solve(inputs):
-    gamma, epsilon = "", ""
     # Some great code from stackOverflow to rotate a list
-    rotatedList = list(zip(*inputs[::-1]))
-    for row in rotatedList:
-        count = Counter(row)
-        gamma += count.most_common(1)[0][0]
-        epsilon += count.most_common(2)[1][0]
-    return int(gamma, 2) * int(epsilon, 2)
+    oxy = ""
+    oxyOptions = deepcopy(inputs)
+    for i in range(len(inputs[0])):
+
+        if len(oxyOptions) == 1:
+            break
+
+        rotatedList = list(zip(*oxyOptions[::-1]))
+        count = Counter(rotatedList[i])
+        if len(count) == 2:
+            most, least = count.most_common(2)
+            if most[1] != least[1]:
+                oxyOptions = list(filter(lambda x: x[i] == most[0], oxyOptions))
+            else:
+                oxyOptions = list(filter(lambda x: x[i] == "1", oxyOptions))
+    oxy = oxyOptions[0]
+
+
+    co = ""
+    coOptions = deepcopy(inputs)
+    for i in range(len(inputs[0])):
+
+        if len(coOptions) == 1:
+            break
+
+        rotatedList = list(zip(*coOptions[::-1]))
+        count = Counter(rotatedList[i])
+        if len(count) == 2:
+            most, least = count.most_common(2)
+            if most[1] != least[1]:
+                coOptions = list(filter(lambda x: x[i] == least[0], coOptions))
+            else:
+                coOptions = list(filter(lambda x: x[i] == "0", coOptions))
+    co = coOptions[0]
+            
+    return int(oxy, 2) * int(co, 2)
 
 
 if __name__ == "__main__":
--- 2021/python/day03_test.py
+++ 2021/python/day03_test.py
@@ -2,4 +2,4 @@ from day03 import solve
 
 def test_solve():
     data = ["00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"]
-    assert solve(data) == 198
+    assert solve(data) == 230
