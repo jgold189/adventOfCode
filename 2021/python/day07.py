f1a61a3db74866b00f70b0a1261f0c91c5b65f55
jordan <climbjn189@gmail.com>
2021-12-07 21:13:09 -0500

    Day 7 Part 2

--- 2021/python/day07.py
+++ 2021/python/day07.py
@@ -1,10 +1,17 @@
 from aocUtil import *
-from statistics import median
+from statistics import mean
+
+def computeFuel(num):
+    return round((num * (num + 1)) / 2)
 
 def solve(inputs):
-    pos = round(median(inputs))
-    fuelCosts = [abs(x - pos) for x in inputs]
-    return sum(fuelCosts)
+    roughPos = round(mean(inputs))
+    potentialFuelCosts = []
+    # The answer should be this position or close by so just check 10 spots around it
+    for pos in range(roughPos - 4, roughPos + 5):
+        fuelCosts = [computeFuel(abs(x - pos)) for x in inputs]
+        potentialFuelCosts.append(sum(fuelCosts))
+    return min(potentialFuelCosts)
 
 
 if __name__ == "__main__":
--- 2021/python/day07_test.py
+++ 2021/python/day07_test.py
@@ -2,4 +2,4 @@ from day07 import solve
 
 def test_solve():
     input = [16,1,2,0,4,2,7,1,2,14]
-    assert solve(input) == 37
+    assert solve(input) == 168
