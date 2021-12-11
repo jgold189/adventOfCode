d4bdc5a573fc71faad511356830c23c21901ce98
jordan <climbjn189@gmail.com>
2021-12-11 17:07:25 -0500

    Day 3 Part 2 Scala

--- 2021/scala/adventofcode/src/main/scala/days/Day03.scala
+++ 2021/scala/adventofcode/src/main/scala/days/Day03.scala
@@ -1,4 +1,5 @@
 import java.lang.Integer.parseInt
+import scala.annotation.tailrec
 
 object Day03 {
 
@@ -9,8 +10,35 @@ object Day03 {
         parseInt(gamma, 2) * parseInt(epsilon, 2)
     }
 
+    def getBitToKeep(countMap: Map[Char, Int], cmp: Char): Char = {
+        cmp match {
+            case '1' => if (countMap.values.toSet.size == 1) cmp else countMap.maxBy(_._2)._1
+            case '0' => if (countMap.values.toSet.size == 1) cmp else countMap.minBy(_._2)._1
+        }
+    }
+
+    // 1 is most, 0 is least
+    @tailrec
+    def filterByBit(remItems: Vector[Vector[Char]], cmp: Char, pos: Int): String = {
+        remItems.size match {
+            case 1 => remItems.apply(0).mkString
+            case _ => {
+                val keepBit = getBitToKeep(remItems.transpose.apply(pos).groupMapReduce(identity)(_ => 1)(_ + _), cmp)
+                filterByBit(remItems.filter(_.apply(pos) == keepBit), cmp, pos + 1)
+            }
+        }
+    }
+
+    def getLifeSupportRating(inputs: Vector[String]): Int = {
+        val vectorizedInput = inputs.map(_.toVector)
+        val oxy = filterByBit(vectorizedInput, '1', 0)
+        val co2 = filterByBit(vectorizedInput, '0', 0)
+        parseInt(oxy, 2) * parseInt(co2, 2)
+    }
+
     def solveDay(): Int = {
-        getGammaEpsilonRate(Util.getInput("03"))
+        // getGammaEpsilonRate(Util.getInput("03"))
+        getLifeSupportRating(Util.getInput("03"))
     }
 
 }
--- 2021/scala/adventofcode/src/test/scala/days/Day03Test.scala
+++ 2021/scala/adventofcode/src/test/scala/days/Day03Test.scala
@@ -1,8 +1,13 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class Day03Test extends AnyFunSuite {
-      test("Day03.") {
+      test("Day03.getGammaEpsilonRate") {
           val data = Vector("00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010")
           assert(Day03.getGammaEpsilonRate(data) === 198)
+      }
+
+      test("Day03.getLifeSupportRating") {
+          val data = Vector("00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010")
+          assert(Day03.getLifeSupportRating(data) === 230)
       }
 }
